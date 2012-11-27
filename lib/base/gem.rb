e31f59e296509e9a2e4a9548b1a11c43560ffb17
Fabio Pelosin <fabiopelosin@gmail.com>
2012-11-27 17:36:53 +0100

    Updated gem template.

--- lib/base/gem.rb
+++ lib/base/gem.rb
@@ -9,10 +9,13 @@ module Pod
 
       attr_reader :gem_spec_file
 
+      attr_reader :root_module
+
       # @param [Pathname] gem_spec_file
       #
-      def initialize(gem_spec_file)
+      def initialize(gem_spec_file, root_module = nil)
         @gem_spec_file = gem_spec_file
+        @root_module = root_module
       end
 
       # @return [Gem::Specification]
@@ -60,9 +63,42 @@ module Pod
         unless @name_spaces
           yard_objects = yard_registry.all(:class, :module).sort_by(&:to_s)
           name_spaces = yard_objects.map { |o| NameSpace.new(o) }
-          @name_spaces = name_spaces.reject { |ns| ns.methods.empty? }
+          @name_spaces = name_spaces.reject do |name_space|
+            name_space.methods.empty? || (!root_module.nil? && name_space.name_as_list.first != root_module)
+          end
         end
           @name_spaces
+      end
+
+      def name_spaces_as_list
+        name_spaces.map(&:parent_name)
+      end
+
+      def name_spaces_tree
+        result = {}
+        name_spaces.each do |name_space|
+          if name_space.root?
+            result[name_space.name] ||= {}
+          else
+            parent_hash = result
+            name_space.name_as_list.each do |name|
+              parent_hash[name] ||= {}
+              parent_hash =  parent_hash[name]
+            end
+          end
+        end
+        result
+      end
+
+      # HELPER
+      def tree_to_html(hash)
+        r = ''
+        hash.each do |key, subhash|
+          r << "<li><p>#{key}</p>"
+          r << "<ul>#{tree_to_html(subhash)}</ul>" unless subhash.empty?
+          r << "</li>"
+        end
+        r
       end
 
       #-----------------------------------------------------------------------#
@@ -111,12 +147,36 @@ module Pod
           @yard_object = yard_object
         end
 
+        def name
+          yard_object.name
+        end
+
+        def root_less_name
+          name_as_list[1..-1] * '::'
+        end
+
+        def children_names
+          children.map(&:name)
+        end
+
+        def parent_name
+          yard_object.parent.name
+        end
+
+        def root?
+          yard_object.parent.is_a?(YARD::CodeObjects::RootObject)
+        end
+
+        def children
+          yard_object.children.select{ |c| c.is_a?(YARD::CodeObjects::NamespaceObject) }
+        end
+
         # @return [Hash{String=>YARD::CodeObjects::MethodObject}]
         #
         def methods_by_group
           result = {}
           methods.each do |method|
-            group = method.group ? method.group.lines.first : 'General'
+            group = method.group ? method.group.lines.first : :no_group
             result[group] ||= []
             result[group] << method
           end
@@ -126,7 +186,7 @@ module Pod
         # @return [Array<String>]
         #
         def groups
-          methods_by_group.keys.sort
+          methods_by_group.keys.reject{ |k| k == :no_group }
         end
 
         # All the methods of the namespace rejecting attributes setters.
@@ -135,7 +195,7 @@ module Pod
         #         namespace
         #
         def methods(included = false)
-          all_methods = yard_object.meths(:visibility => :public, :scope => :instance, :included => included )
+          all_methods = yard_object.meths(:visibility => :public, :scope => :instance, :included => included)
           method_names = all_methods.map { |m| m.name.to_s }
           # Attributes
           methods = all_methods.reject do |method|
@@ -159,7 +219,7 @@ module Pod
           methods_height   = methods_by_group.values.flatten.count * method_height
           total_height     = groups_height + methods_height
           height_percolumn = total_height / max_columns
-          height_percolumn = 44 unless height_percolumn > 44
+          # height_percolumn = 44 unless height_percolumn > 44
 
           groups_and_methods = []
           methods_by_group.each do |group, methods|
@@ -172,6 +232,7 @@ module Pod
           column_height = 0
           columns[0] = []
           groups_and_methods.each do |entry|
+            next if entry == :no_group
             if column_height >= height_percolumn
               column_height = 0
               current_column += 1
@@ -191,6 +252,14 @@ module Pod
 
         def to_s
           yard_object.to_s
+        end
+
+        def name_as_list
+          yard_object.to_s.split('::')
+        end
+
+        def parent_names
+          name_as_list[0..-1]
         end
 
       end
